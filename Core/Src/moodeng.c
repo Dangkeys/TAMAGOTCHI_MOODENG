4bef93b7542c254b23f3ca60f102cb13c194db69
name <joyyer02@gmail.com>
2025-10-14 13:19:59 +0700

    Update moodeng.c

--- Core/Src/moodeng.c
+++ Core/Src/moodeng.c
@@ -547,7 +547,7 @@ void Moodeng_Handle_Lose(Moodeng_t* moodeng)
 void Moodeng_lose_animation(Moodeng_t* moodeng)
 {
     if (moodeng->isAlive == false) {
-        if (losing && HAL_GetTick() - loseStart >= 2000) {
+        if (losing && HAL_GetTick() - loseStart >= 150) {
             losing = false;
             Moodeng_Reset(moodeng);
             Timer_Reset(&gameClock);
